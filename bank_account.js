37d2801bf4a26bfdc1e4721f36843cfedcc447bf
Usman Hidayatulloh Sidiq <usmanhdsidiq@gmail.com>
2024-09-14 18:39:08 +0700

    Menambah class
    Terlanjur mengumpulkan, ternyata yang sebelumnya belum ada class-nya .

--- bank_account.js
+++ bank_account.js
@@ -1,21 +1,45 @@
-let saldo = 5000;
+class BankAccount {
+    constructor(jumlahSaldo) {
+        this.saldo = jumlahSaldo;
+    }
 
-document.getElementById("saldo").innerText = saldo;
+    getSaldo() { // Method untuk mengembalikan nilai saldo
+        return this.saldo;
+    }
 
-document.getElementById("kurangiSaldo").onclick = function() { // Fungsi untuk mengurangi saldo
-    let promptKurang = window.prompt("Masukkan nominal yang ingin anda kurangi");
-
-    if(promptKurang > saldo) {
-        window.alert("Saldo tidak cukup"); // Untuk menampilkan alert jika nominal yang dimasukkan lebih besar daripada jumlah saldo
-    } else {
-        saldo -= parseInt(promptKurang);
-        document.getElementById("saldo").innerText = saldo;
+    setSaldo(newSaldo) { // Method untuk memperbarui nilai saldo
+        this.saldo = newSaldo;
     }
 }
 
-document.getElementById("tambahSaldo").onclick = function() { // Fungsi untuk menambah saldo
-    let promptTambah = window.prompt("Masukkan nominal yang akan ditambahkan");
+function tampilkanSaldo(bankAccount) { // Function untuk menampilkan nilai saldo
+    document.getElementById("saldo").innerText = bankAccount.getSaldo();
+}
 
-    saldo += parseInt(promptTambah);
-    document.getElementById("saldo").innerText = saldo;
+function kurangiSaldo(bankAccount, nominal) { // Function untuk mengurangi nilai saldo
+    if(nominal > bankAccount.getSaldo()) {
+        window.alert("Saldo tidak cukup"); // Untuk menampilkan alert jika nominal yang dimasukkan lebih besar daripada jumlah saldo
+    } else {
+        // Mengurangi jumlah saldo berdasarkan nominal yang dimasukkan
+        bankAccount.setSaldo(bankAccount.getSaldo() - nominal);
+        tampilkanSaldo(bankAccount);
+    }
+}
+
+function tambahSaldo(bankAccount, nominal) { // Function untuk menambah nilai saldo
+    bankAccount.setSaldo(bankAccount.getSaldo() + nominal);
+    tampilkanSaldo(bankAccount);
+}
+
+let bankAccount = new BankAccount(5000); // Menentukan nilai awal saldo
+tampilkanSaldo(bankAccount);
+
+document.getElementById("kurangiSaldo").onclick = function() { 
+    let promptKurang = parseInt(window.prompt("Masukkan nominal yang ingin anda kurangi"));
+    kurangiSaldo(bankAccount, promptKurang);
+}
+
+document.getElementById("tambahSaldo").onclick = function() {
+    let promptTambah = parseInt(window.prompt("Masukkan nominal yang akan ditambahkan"));
+    tambahSaldo(bankAccount, promptTambah);
 }
